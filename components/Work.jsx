345baeb48da16287bff5746e151ffbc6b0067b33
Samson Sham <samsonsham@gmail.com>
2022-02-16 20:25:13 +0000

    add project-1 (Noughts and Crosses) to Work Sectio

--- components/Work.jsx
+++ components/Work.jsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router';
 // i18n
 import { useTranslation } from 'next-i18next';
 
-// Next
+// Chakra
 import {
   Stack,
   Container,
@@ -13,14 +13,22 @@ import {
   Text,
   Heading,
   SimpleGrid,
+  ButtonGroup, 
+	IconButton,
+  useColorModeValue,
+  Image
 } from '@chakra-ui/react';
+
+// Icons
+import { FaExternalLinkAlt, FaGithub } from 'react-icons/fa';
 
 export default function Work() {
   const { t } = useTranslation('common');
 	const router = useRouter();
-	const getStats = () =>{
-		return router.locale === 'en' ? stats_en : stats_tc
-	}
+  const iconFontSize = "30px";
+  const iconColor = useColorModeValue('gray.600', 'gray.600');
+	const iconHoverColor = useColorModeValue('blackAlpha.600', 'gray.400');
+
   return (
     <Box bg={''} position={'relative'}>
       <Flex
@@ -47,7 +55,8 @@ export default function Work() {
               <Heading
                 color={'gray.500'}
                 mb={5}
-                fontSize={{ base: '3xl', md: '5xl' }}>
+                fontWeight={800}
+                fontSize={{ base: '4xl', md: '6xl' }}>
                 {t('my-project')}
               </Heading>
               <Text as={'span'} fontSize={'xl'} color={'gray.400'}>
@@ -56,24 +65,56 @@ export default function Work() {
               </Text>
             </Box>
 
-            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
-              {getStats().map((stat) => (
+            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4}>
+              {stats.map((stat, i) => (
                 <Box 
-                    key={stat.title}
+                    key={i}
                     bg={'gray.100'}
                     rounded="xl"
                     p={4}
                 >
                   <Text
-                    fontFamily={'heading'}
+                    fontFamily={'Times New Roman'}
+                    fontWeight={900}
                     fontSize={'3xl'}
-                    color={''}
+                    color={'gray.600'}
                     mb={3}>
-                    {stat.title}
+                    {router.locale === 'en' ? stat.title_en : stat.title_tc}
                   </Text>
-                  <Text fontSize={'xl'} color={'gray.500'}>
-                    {stat.content}
+                  <Image
+                    borderRadius='10px'
+                    boxSize='110px'
+                    float='right'
+                    ml={2}
+                    objectFit='cover'
+                    src={stat.imgSrc}
+                    alt={router.locale === 'en' ? stat.title_en : stat.title_tc}
+                  />
+                  <Text fontSize={'md'} color={'gray.700'} textAlign='left'>
+                    {stat.description}
                   </Text>
+                  
+                  <Stack direction={'row'} my={4}>
+                    <ButtonGroup variant="ghost" m={0} display={'inline-flex'} >
+                      <IconButton color={iconColor} _hover={{ color: iconHoverColor }} mx={0} as="a" href={stat.githubLink} aria-label="GitHub" icon={<FaGithub fontSize={iconFontSize} />} />
+                      <IconButton color={iconColor} _hover={{ color: iconHoverColor }} mx={0} as="a" href={stat.siteLink} aria-label="Twitter" icon={<FaExternalLinkAlt fontSize={iconFontSize} />} />
+                    </ButtonGroup>
+                  </Stack>
+                  <Stack direction={'row'} my={2} bottom={0} position='static'>
+                  {stat.builtWith.map((tech, i)=>(
+                    <Text
+                      key={i}
+                      bg={'gray.500'}
+                      color={'white'}
+                      fontSize={'sm'}
+                      px={1}
+                      py={0}
+                      borderRadius={5}
+                    >
+                      {tech}
+                    </Text>
+                  ))}
+                  </Stack>
                 </Box>
               ))}
             </SimpleGrid>
@@ -84,78 +125,41 @@ export default function Work() {
   );
 }
 
-const StatsText = ({ children }) => (
-  <Text as={'span'} fontWeight={700} color={''}>
-    {children}
-  </Text>
-);
-
-const stats_en = [
+const stats = [
   {
-    title: 'Sample Project 1',
-    content: (
-      <>
-        <StatsText>Sample Text</StatsText> Coming Soon
-      </>
-    ),
+    title_en: 'Noughts and Crosses',
+    title_tc: '\u904e\u4e09\u95dc',
+    imgSrc: 'https://ik.imagekit.io/c5xc1x6srka/screenshot/screen_s_PYdH0GD1o?ik-sdk-version=javascript-1.4.3&updatedAt=1644969260245',
+    description: 'A.k.a. Tic-tac-toe. My very first project.',
+    githubLink: 'https://github.com/samsonsham/noughts-and-crosses',
+    siteLink: 'https://samsonsham.github.io/noughts-and-crosses/',
+    builtWith: ['React','Typescript','SASS'],
   },
   {
-    title: 'Sample Project 2',
-    content: (
-      <>
-        <StatsText>Sample Text</StatsText> Coming Soon
-      </>
-    ),
+    title_en: 'Sample Project 2',
+    title_tc: '\u7bc4\u4f8b\u4f5c\u54c1 2',
+    imgSrc: '',
+    description: '',
+    githubLink: '',
+    siteLink: '',
+    builtWith: [],
   },
   {
-    title: 'Sample Project 3',
-    content: (
-      <>
-        <StatsText>Sample Text</StatsText> Coming Soon
-      </>
-    ),
+    title_en: 'Sample Project 3',
+    title_tc: '\u7bc4\u4f8b\u4f5c\u54c1 2',
+    imgSrc: '',
+    description: '',
+    githubLink: '',
+    siteLink: '',
+    builtWith: [],
   },
   {
-    title: 'Sample Project 4',
-    content: (
-      <>
-        <StatsText>Sample Text</StatsText> Coming Soon
-      </>
-    ),
-  },
-];
-
-const stats_tc = [
-  {
-    title: '\u7bc4\u4f8b\u4f5c\u54c1 1',
-    content: (
-      <>
-        <StatsText>&#x7BC4;&#x4F8B;&#x6587;&#x5B57;</StatsText> &#x656C;&#x8ACB;&#x671F;&#x5F85;
-      </>
-    ),
-  },
-  {
-    title: '\u7bc4\u4f8b\u4f5c\u54c1 2',
-    content: (
-      <>
-        <StatsText>&#x7BC4;&#x4F8B;&#x6587;&#x5B57;</StatsText> &#x656C;&#x8ACB;&#x671F;&#x5F85;
-      </>
-    ),
-  },
-  {
-    title: '\u7bc4\u4f8b\u4f5c\u54c1 3',
-    content: (
-      <>
-        <StatsText>&#x7BC4;&#x4F8B;&#x6587;&#x5B57;</StatsText> &#x656C;&#x8ACB;&#x671F;&#x5F85;
-      </>
-    ),
-  },
-  {
-    title: '\u7bc4\u4f8b\u4f5c\u54c1 4',
-    content: (
-      <>
-        <StatsText>&#x7BC4;&#x4F8B;&#x6587;&#x5B57;</StatsText> &#x656C;&#x8ACB;&#x671F;&#x5F85;
-      </>
-    ),
+    title_en: 'Sample Project 4',
+    title_tc: '\u7bc4\u4f8b\u4f5c\u54c1 2',
+    imgSrc: '',
+    description: '',
+    githubLink: '',
+    siteLink: '',
+    builtWith: [],
   },
 ];
